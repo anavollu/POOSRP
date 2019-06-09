39af2938afa8a6537156ca726e4e8d82a8aee769
eduardolagoeiro <eduardolagoeiro@id.uff.br>
2019-06-09 11:26:17 -0300

    complete

--- src/pooscrp/POOSCRP.java
+++ src/pooscrp/POOSCRP.java
@@ -1,21 +1,31 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package pooscrp;
 
-/**
- *
- * @author lagoeiro
- */
+import dao.EmployeeDAO;
+import models.Employee;
+import reports.EmployeeReport;
+
 public class POOSCRP {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Employee emp1 = new Employee(1L, "joao", "finanças", true);
+        Employee emp2 = new Employee(2L, "carlos", "finanças", true);
+        Employee emp3 = new Employee(3L, "bruno", "finanças", true);
+        Employee emp4 = new Employee(4L, "marcelo", "finanças", true);
+        EmployeeReport empReport = new EmployeeReport();
+        System.out.println("Print XML REPORT employee 1");
+        empReport.printEmployeeDetailReportXML(emp1);
+        System.out.println("\nPrint XML REPORT employee 2");
+        empReport.printEmployeeDetailReportCSV(emp2);
+        EmployeeDAO empDao = new EmployeeDAO();
+        empDao.saveEmployeeToDatabase(emp1);
+        empDao.saveEmployeeToDatabase(emp2);
+        empDao.saveEmployeeToDatabase(emp3);
+        empDao.saveEmployeeToDatabase(emp4);
+        System.out.println("\nPrint fake database with all employees");
+        System.out.println(EmployeeDAO.employees);
+        empDao.terminateEmployee(1L);
+        System.out.println("\nPrint fake database with all employees after remove employee 1");
+        System.out.println(EmployeeDAO.employees);
     }
-    
+
 }
